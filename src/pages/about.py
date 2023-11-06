6b6d6ebfce416bddf0399d92184523ccd5dbb2e1
jyablonski9 <jyablonski9@gmail.com>
2023-11-06 12:55:53 -0600

    Bump version: 1.0.3 → 1.0.4

--- src/pages/about.py
+++ src/pages/about.py
@@ -105,7 +105,7 @@ about_layout = html.Div(
             ]
         ),
         html.Br(),
-        html.P("Version: 1.0.3"),
+        html.P("Version: 1.0.4"),
     ],
     className="custom-padding",
 )
