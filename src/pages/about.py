17386de8c14c8cda5f1e305449646374ff34f652
jyablonski9 <jyablonski9@gmail.com>
2024-08-17 08:37:33 -0700

    Bump version: 1.0.13 → 1.0.14

--- src/pages/about.py
+++ src/pages/about.py
@@ -110,7 +110,7 @@ about_layout = html.Div(
         html.P(
             f"Dashboard Last Refreshed {datetime.now().strftime('%A, %B %d %-I:%M %p UTC')}"
         ),
-        html.P("Version: 1.0.13"),
+        html.P("Version: 1.0.14"),
     ],
     className="custom-padding",
 )
